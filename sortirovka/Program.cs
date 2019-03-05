e64e5709a960d2f8ce81cc1c7249ffc12eb8bc49
brykin <alex.engeneer90@gmail.com>
2019-03-05 18:49:46 +0300

    Добавлены проверки на коректные символы и длину массива

--- sortirovka/Program.cs
+++ sortirovka/Program.cs
@@ -10,23 +10,56 @@ namespace sortirovka
     class Program
     {
         private const int arrayLenght = 10;
-        private static Regex rg = new Regex("[ -0123456789]");
-        public static bool isNumericString(string strToCheck)
+        private static Regex rg = new Regex("^[0-9 -]+$");
+
+        public static bool IsTenNumbers(string strToCheck)
+        {
+            string[] result = strToCheck.Split(' ');
+
+            if (result.Length != 10)
+            {
+                Console.WriteLine("Введено " + result.Length + " чисел");
+                return false;
+            }
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (!int.TryParse(result[i], out int a))
+                {
+                    Console.WriteLine("Строка " + result[i] + " не является числом");
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool IsNumericString(string strToCheck)
         {
             return rg.IsMatch(strToCheck);
         }
+
         static void Main(string[] args)
         {
-
             string input = string.Empty;
+            bool check = false;
             Console.WriteLine("Введите " + arrayLenght + " натуральных целых чисел через пробел");
             do
             {
-                //input = Console.Read();           //считываем ввод пользователя
-                char symb = (char)Console.Read();
+                input = Console.ReadLine();             //считываем ввод пользователя
 
+                if (!IsNumericString(input))
+                {
+                    Console.WriteLine("Используйте только цифры, знак минуса и пробелы");
+                    continue;
+                }
+
+                if (IsTenNumbers(input))
+                {
+                    check = true;
+                }
             }
-            while ();
+            while (!check);
             string[] numbers = input.Split(' ');        //создали массив стрингов и записили в массив значения введёные пользователем
             int[] sequence = new int[arrayLenght];
 
